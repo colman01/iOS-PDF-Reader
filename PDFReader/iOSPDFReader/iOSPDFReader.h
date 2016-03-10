5dce024ddc15c9903659512841f143149020040c
Dirk Koller <dirk.koller@dvag.com>
2016-03-10 12:21:37 +0100

    Added main document view controller to header umbrella

--- PDFReader/iOSPDFReader/iOSPDFReader.h
+++ PDFReader/iOSPDFReader/iOSPDFReader.h
@@ -14,4 +14,4 @@ FOUNDATION_EXPORT const unsigned char iOSPDFReaderVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <iOSPDFReader/PublicHeader.h>
 
-
+#import <PDFReaderFramework/CPDFDocumentViewController.h>
