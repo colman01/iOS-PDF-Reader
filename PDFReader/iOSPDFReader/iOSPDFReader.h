77f3354bd4761a3d6ba8d530219985bffb2e0abb
Dirk Koller <dirk.koller@dvag.com>
2016-03-10 11:59:18 +0100

    Added cocoa touch framework

---
+++ PDFReader/iOSPDFReader/iOSPDFReader.h
@@ -0,0 +1,17 @@
+//
+//  iOSPDFReader.h
+//  iOSPDFReader
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSPDFReader.
+FOUNDATION_EXPORT double iOSPDFReaderVersionNumber;
+
+//! Project version string for iOSPDFReader.
+FOUNDATION_EXPORT const unsigned char iOSPDFReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSPDFReader/PublicHeader.h>
+
+
